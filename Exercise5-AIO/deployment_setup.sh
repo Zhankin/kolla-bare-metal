e0e8ee6ef71a44e0713bf870f241843d00546ac3
Zhankin <zhankin@mail.ru>
2019-06-18 15:17:55 +0600

    Update deployment_setup.sh

--- Exercise5-AIO/deployment_setup.sh
+++ Exercise5-AIO/deployment_setup.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-cp /vagrant/deployment_interfaces /etc/network/interfaces
-cp /vagrant/hosts /etc/hosts
-cp /vagrant/grub /etc/default/grub
+#cp /vagrant/deployment_interfaces /etc/network/interfaces
+#cp /vagrant/hosts /etc/hosts
+#cp /vagrant/grub /etc/default/grub
 
 update-grub
 
@@ -13,16 +13,16 @@ apt install -y python-jinja2 python-pip libssl-dev
 apt install -y lvm2 thin-provisioning-tools curl
 pip install -U pip
 
-pvcreate /dev/sdc
-vgcreate cinder-volumes /dev/sdc
+#pvcreate /dev/sdc
+#vgcreate cinder-volumes /dev/sdc
 
 echo "configfs" >> /etc/modules
 update-initramfs -u
 systemctl daemon-reload
 
-mkdir -p /home/vagrant/kolla
-cp /vagrant/globals.yml /home/vagrant/kolla
-cp /vagrant/run-kolla.sh /home/vagrant/kolla
-cp /vagrant/init-runonce /home/vagrant/kolla
+#mkdir -p /home/vagrant/kolla
+#cp /vagrant/globals.yml /home/vagrant/kolla
+#cp /vagrant/run-kolla.sh /home/vagrant/kolla
+#cp /vagrant/init-runonce /home/vagrant/kolla
 
 reboot
